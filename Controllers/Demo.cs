5b82d9a69f62044b7a2f36e1b5abd80df7c39d9b
dhimo simoni <dhimo-simoni@hotmail.com>
2020-09-27 01:31:11 +0200

    Update 2

--- Controllers/Demo.cs
+++ Controllers/Demo.cs
@@ -1,107 +1,26 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using JustScan.Data;
+using JustScan.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace JustScan.Controllers
 {
     public class DemoController : Controller
     {
-        IConfiguration _iconfiguration;
+        private readonly IConfiguration _iconfiguration;
+
         public DemoController(IConfiguration iconfiguration)
         {
             _iconfiguration = iconfiguration;
         }
-        // GET: Demo
         public ActionResult Index()
-        {
-            //string connectionString = _iconfiguration.GetSection("ConnectionStrings").GetSection("ConnectionString").Value;
+        { 
 
-            //using (SqlConnection  sqlCon = new SqlConnection(connectionString))
-            //{
-            //    sqlCon.Open();
-            //    SqlCommand sql_cmnd = new SqlCommand("PROC_NAME", sqlCon);
-            //    //sql_cmnd.CommandType = CommandType.StoredProcedure;
-            //    //sql_cmnd.Parameters.AddWithValue("@FIRST_NAME", SqlDbType.NVarChar).Value = firstName;
-            //    //sql_cmnd.Parameters.AddWithValue("@LAST_NAME", SqlDbType.NVarChar).Value = lastName;
-            //    //sql_cmnd.Parameters.AddWithValue("@AGE", SqlDbType.Int).Value = age;
-            //    sql_cmnd.ExecuteNonQuery();
-            //    sqlCon.Close();
-            //}
-            return View();
+            Database logic = new Database(_iconfiguration);
+            return View(logic.getMenu());
         }
 
-        // GET: Demo/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Demo/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Demo/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Demo/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Demo/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Demo/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Demo/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+       
     }
 }
---
+++ Models/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JustScan.Models
+{
+    public class Menu
+    {
+        public string BusinessType { get; set; }
+
+        public string BusinessName { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+
+    }
+}
